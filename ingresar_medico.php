5e1bc38feb13dbd06df1f8b5338932202313c500
MARTINEZCANCHON <jcmartinez103@misena.edu.co>
2016-07-14 11:10:55 -0500

    Update ingresar_medico.php
    Comentare el código

--- ingresar_medico.php
+++ ingresar_medico.php
@@ -4,6 +4,7 @@
  * @author gencyolcu
  * @copyright 2016
  */
+ //En esta parte del codigo se instancia a pacientes en un objeto llamado "p" y en este objeto se llama al metodo "registar Medico"
 include('Class/motor.php');
 $cedula=$_GET['cedula'];
 $p = new pacientes();
