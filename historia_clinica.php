e85acc3943600f134f6a7c7f14b5dacce19fcace
MARTINEZCANCHON <jcmartinez103@misena.edu.co>
2016-07-14 11:18:16 -0500

    Update historia_clinica.php
    Comentare  el codigo

--- historia_clinica.php
+++ historia_clinica.php
@@ -1,11 +1,13 @@
 <?php
-
+//Se utiliza el include para llamar a el archivo maotor.php
 include ("Class/motor.php");
-
+//Se recibe una variable por el metodo GET y se asigna a la variable $cedula
 $cedula=$_GET['var'];
 
+//Se instancia la clase medico 
 $medico=new medico();
 echo "<br>";
+//Se llama el metodo Historia_Clinica
 $medico->Historia_clinica($cedula);
 
 
